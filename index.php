8279e9472848b89da88618f41e263e4c841993bd
Majos73 <100152096+Majos73@users.noreply.github.com>
2022-03-31 13:58:55 +0200

    Login 60%
    Diviso richiesta otpCode da login (inserimento mail). Manca invio e controllo.

--- index.php
+++ index.php
@@ -11,32 +11,25 @@
 </head>
 
 <body>
+    <form action="loginOTP.php" method="POST" class="mb-3" onsubmit="return OTPCode();">
+        <h1>Login</h1>
+        <input type="mail" name="mail" id="mail" placeholder="Email" class="form-control w-75" pattern="(?:prof|ata|)[a-z0-9.]{2,61}@darzo.net" required>
+        <input type="submit" value="Invio" class="form-control w-25" style="background-color: #ddd;" />
+    </form>
+    <button class="btn bottom">Guest</button>
+
     <script>
         var flagCodice = false;
 
         function OTPCode() {
-            if (!flagCodice) {
-                var n = document.getElementById(mail).value;
+            if (!flagCodice) { 
+                
                 console.log(n);
-                //document.getElementById(mail).setAttribute('disabled', 'disabled');
-                //document.getElementById(otp).removeAttribute('disabled');
-                window.onbeforeunload = function() {
-                    return null;
-                }
                 return false;
             }
-            return true;
-
+            return false;
         }
     </script>
-
-    <form class="mb-3" onsubmit="return OTPCode();">
-        <h1>Login</h1>
-        <input type="mail" name="mail" id="mail" placeholder="Email" class="form-control w-75" pattern="(?:prof|ata)[a-z0-9.]{2,61}@darzo.net" required>
-        <input class="form-control w-75" type="text" id="otp" placeholder="Codice OTP" disabled>
-        <input type="submit" value="Invio" class="form-control w-25" style="background-color: #ddd;" />
-    </form>
-    <button class="btn bottom">Guest</button>
 </body>
 
 </html>
---
+++ loginOTP.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="grafica.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="<?$_SERVER['PHP_SELF']?>">
+        <h1>Login</h1>
+        <input class="form-control w-75 bottom" type="text" id="otp" placeholder="Inserire il codice OTP ricevuto via mail" >
+        <input type="submit" value="Invio" class="form-control w-25 top" style="background-color: #ddd;" />
+    </form>
+</body>
+</html>
