aa28adf267290c3882b605c703e1e3a1950a39c4
Majos73 <100152096+Majos73@users.noreply.github.com>
2022-04-12 15:58:00 +0200

    Login 90%

--- pages/header.php
+++ pages/header.php
@@ -7,3 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../grafica.css">
+    <script>
+        function noSession (){
+            alert("Purtroppo non hai ancora effettuato l'accesso, verrai reindirizzato alla pagina per il login");
+        }
+    </script>
--- pages/homepage.php
+++ pages/homepage.php
@@ -1,2 +1,21 @@
-<?php
+<?php 
+include("header.php");
 ?>
+</head>
+
+<?php
+session_start();
+echo($_SESSION['mail']);
+if($_SESSION['mail']==null){
+    echo "<script>noSession();</script>";
+    session_abort();
+    header("refresh:0;url=../index.php");
+}
+
+if($_)
+?>
+
+<body>
+    
+</body>
+</html>
