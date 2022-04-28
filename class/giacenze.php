05ccafffbfa493561c7e4968ba45f349278cf8eb
Majos73 <100152096+Majos73@users.noreply.github.com>
2022-04-28 11:07:00 +0200

    Continuo funzionalità
    Tempo 3h

---
+++ class/giacenze.php
@@ -0,0 +1,179 @@
+<?php
+class Giacenze
+{
+    // Connection
+    private $conn;
+    // Table
+    private $db_table = "giacenze";
+    // Columns
+    public $id;
+    public $componente;
+    public $sigla;
+    public $valore;
+    public $umValore;
+    public $valore2;
+    public $umValore2;
+    public $note;
+    public $immagine;
+    public $quantitaMin;
+    public $id_categoria;
+    public $id_catalogo;
+    // Db connection
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    // CREATE
+    public function createComponente()
+    {
+        $sqlQuery = "INSERT INTO " . $this->db_table . "
+                    SET
+                        nomeComp = ?,
+                        sigla = ?,
+                        valore = ?,
+                        umValore = ?,
+                        valore2 = ?,
+                        umValore2 = ?,
+                        note = ?,
+                        immagine = ?,
+                        quantitaMin = ?,
+                        ID_Categoria = ?,
+                        ID_Catalogo = ?";
+
+        $stmt = $this->conn->prepare($sqlQuery);
+
+        // sanitize
+        $this->componente = htmlspecialchars(strip_tags($this->componente));
+        $this->sigla = htmlspecialchars(strip_tags($this->sigla));
+        $this->valore = htmlspecialchars(strip_tags($this->valore));
+        $this->umValore = htmlspecialchars(strip_tags($this->umValore));
+        $this->valore2 = htmlspecialchars(strip_tags($this->valore2));
+        $this->umValore2 = htmlspecialchars(strip_tags($this->umValore2));
+        $this->note = htmlspecialchars(strip_tags($this->note));
+        $this->immagine = htmlspecialchars(strip_tags($this->immagine));
+        $this->quantitaMin = htmlspecialchars(strip_tags($this->quantitaMin));
+        $this->id_categoria = htmlspecialchars(strip_tags($this->id_categoria));
+        $this->id_catalogo = htmlspecialchars(strip_tags($this->id_catalogo));
+
+        $stmt->bindParam(1, $this->componente);
+        $stmt->bindParam(2, $this->sigla);
+        $stmt->bindParam(3, $this->valore);
+        $stmt->bindParam(4, $this->umValore);
+        $stmt->bindParam(5, $this->valore2);
+        $stmt->bindParam(6, $this->umValore2);
+        $stmt->bindParam(7, $this->note);
+        $stmt->bindParam(8, $this->immagine);
+        $stmt->bindParam(9, $this->quantitaMin);
+        $stmt->bindParam(10, $this->id_categoria);
+        $stmt->bindParam(11, $this->id_catalogo);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function getComponentiPerTable()
+    {
+        $sqlQuery = "SELECT
+                        ID_Componente, nomeComp, sigla, ID_Categoria, ID_Catalogo
+                      FROM
+                        " . $this->db_table;
+        $stmt = $this->conn->prepare($sqlQuery);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function getComponenti()
+    {
+        $sqlQuery = "SELECT
+                        ID_Componente, nomeComp, sigla, valore, umValore, valore2, umValore2, note, immagine, quantitaMin, ID_Categoria, ID_Catalogo
+                      FROM
+                        " . $this->db_table;
+        $stmt = $this->conn->prepare($sqlQuery);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function getSingleComponente()
+    {
+        $sqlQuery = "SELECT
+                        nomeComp, sigla, valore, umValore, valore2, umValore2, note, immagine, quantitaMin, ID_Categoria, ID_Catalogo
+                      FROM
+                        " . $this->db_table . " WHERE ID_Componente = ?";
+        $stmt = $this->conn->prepare($sqlQuery);
+        $stmt->bindParam(1, $this->id);
+        $stmt->execute();
+        $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $dataRow;
+    }
+
+    public function updateComponente()
+    {
+        $sqlQuery = "UPDATE
+                        " . $this->db_table . "
+                    SET
+                        nomeComp = ?, 
+                        sigla = ?,
+                        valore = ?, 
+                        umValore = ?, 
+                        valore2 = ?, 
+                        umValore2 = ?, 
+                        note = ?, 
+                        immagine = ?, 
+                        quantitaMin = ?, 
+                        ID_Categoria = ?, 
+                        ID_Catalogo = ?
+                    WHERE 
+                        ID_Componente = ?";
+
+        $stmt = $this->conn->prepare($sqlQuery);
+
+        $this->componente = htmlspecialchars(strip_tags($this->componente));
+        $this->sigla = htmlspecialchars(strip_tags($this->sigla));
+        $this->valore = htmlspecialchars(strip_tags($this->valore));
+        $this->umValore = htmlspecialchars(strip_tags($this->umValore));
+        $this->valore2 = htmlspecialchars(strip_tags($this->valore2));
+        $this->umValore2 = htmlspecialchars(strip_tags($this->umValore2));
+        $this->note = htmlspecialchars(strip_tags($this->note));
+        $this->immagine = htmlspecialchars(strip_tags($this->immagine));
+        $this->quantitaMin = htmlspecialchars(strip_tags($this->quantitaMin));
+        $this->id_categoria = htmlspecialchars(strip_tags($this->id_categoria));
+        $this->id_catalogo = htmlspecialchars(strip_tags($this->id_catalogo));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $stmt->bindParam(1, $this->componente);
+        $stmt->bindParam(2, $this->sigla);
+        $stmt->bindParam(3, $this->valore);
+        $stmt->bindParam(4, $this->umValore);
+        $stmt->bindParam(5, $this->valore2);
+        $stmt->bindParam(6, $this->umValore2);
+        $stmt->bindParam(7, $this->note);
+        $stmt->bindParam(8, $this->immagine);
+        $stmt->bindParam(9, $this->quantitaMin);
+        $stmt->bindParam(10, $this->id_categoria);
+        $stmt->bindParam(11, $this->id_catalogo);
+        $stmt->bindParam(12, $this->id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    function deleteComponente()
+    {
+        $sqlQuery = "DELETE FROM " . $this->db_table . " WHERE ID_Componente = ?";
+        $stmt = $this->conn->prepare($sqlQuery);
+
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $stmt->bindParam(1, $this->id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+}
--- pages/componenti/visualizzaComp.php
+++ pages/componenti/visualizzaComp.php
@@ -1,6 +1,27 @@
 <?php
 include("../header.php");
+include_once("/xampp/htdocs/GestionalePHP/class/componenti.php");
+include_once("/xampp/htdocs/GestionalePHP/class/giacenze.php");
+include_once("/xampp/htdocs/GestionalePHP/class/categorie.php");
+include_once("/xampp/htdocs/GestionalePHP/class/cataloghi.php");
+include_once '/xampp/htdocs/GestionalePHP/config/database.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$oggComp = new Componenti($db);
+$oggGiac = new Giacenze($db);
+$oggCateg = new Categorie($db);
+$oggCatal = new Cataloghi($db);
+
+$oggComp->id = $_GET['id'];
+$datiComponenti = $oggComp->getSingleComponente();
+
+$oggGiac->id_componente = $_GET['id'];
+$oggCateg->id = $datiComponenti['ID_Categoria'];
+$oggCatal->id = $datiComponenti['ID_Catalogo'];
 ?>
+
 <title>Componente <?= $_GET['id'] ?></title>
 </head>
 
@@ -9,11 +30,109 @@ include("../header.php");
     include("../firstNavbar.php");
     ?>
     <nav>
-        <button data-toggle="modal" data-target="#aggiungiLocale">Aggiungi Locale <i class="fa-solid fa-plus"></i></button>
+        <button data-toggle="modal" data-target="#aggiungiGiacenza">Aggiungi Giacenza <i class="fa-solid fa-plus"></i></button>
     </nav>
     <?php
     include("../secondNavbar.php");
     ?>
+
+    <div class="container-dati">
+        <div class="dati">
+            <h3>Componente <?= $datiComponenti['nomeComp'] ?> &nbsp
+                <a href="#" data-toggle="modal" data-target="#modalImage">
+                    <i class="fa-solid fa-image"></i>
+                </a>
+            </h3>
+            <p>Sigla = <?= $datiComponenti['sigla'] ?></p>
+            <p>Categoria = <?= $oggCateg->getSingleCategoria() ?></p>
+            <p>Primo valore = <?= $datiComponenti['valore'] ?> <?= $datiComponenti['umValore'] ?></p>
+            <p>Secondo valore = <?= $datiComponenti['valore2'] ?> <?= $datiComponenti['umValore2'] ?> </p>
+            <?php
+            if ($datiComponenti['note']) {
+            ?>
+                <p>Note = <?= $datiComponenti['note'] ?></p>
+            <?php
+            } else {
+            ?><p>Note = ---</p>
+            <?php
+            }
+            ?>
+            <p>Quantità minima = <?= $datiComponenti['quantitaMin'] ?></p>
+            <p>Catalogo = <?= $oggCatal->getSingleCatalogo() ?></p>
+        </div>
+    </div>
+    
+    <div class="container-giacenze">
+        <div class="container-table">
+            <table id="example" class="display">
+                <thead class="dark">
+                    <tr>
+                        <th>ID</th>
+                        <th>Componente</th>
+                        <th>Sigla</th>
+                        <th>Categoria</th>
+                        <th>Catalogo</th>
+                        <th>Opzioni</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $itemCount = $datiComponenti->rowCount();
+
+                    if ($itemCount > 0) {
+                        $x = 1;
+                        while ($row = $datiComponenti->fetch(PDO::FETCH_ASSOC)) {
+                            extract($row);
+                            $oggCatal->id = $row['ID_Catalogo'];
+                            $oggCateg->id = $row['ID_Categoria'];
+                    ?>
+                            <tr>
+                                <td> <?= $x ?> </td>
+                                <td> <?= $row['nomeComp'] ?></td>
+                                <td> <?= $row['sigla'] ?> </td>
+                                <td> <?= $oggCateg->getSingleCategoria() ?> </td>
+                                <td> <?= $oggCatal->getSingleCatalogo() ?> </td>
+                                <td>
+                                    <a onclick="inviaID(this); ottieniDati()" data-toggle="modal" data-target="#modificaComponente" id="<?= $row['ID_Componente'] ?>">
+                                        <i class="fa-solid fa-pencil"></i>
+                                    </a>
+                                    <a onclick="eliminazione(this)" id="<?= $row['ID_Componente'] ?>">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </a>
+                                    <a onclick="visualizza(this)" id="<?= $row['ID_Componente'] ?>">
+                                        <i class="fa-solid fa-eye"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php
+                            $x++;
+                        }
+                    } else {
+                        ?>
+                        <tr>
+                            <td> </td>
+                            <td> </td>
+                            <td> </td>
+                            <td> </td>
+                            <td> </td>
+                            <td> </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+    <div class="modal fade" id="modalImage" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <img src="<?= $datiComponenti['immagine'] ?>" alt="Immagine Componente <?= $datiComponenti['nomeComp'] ?>">
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
